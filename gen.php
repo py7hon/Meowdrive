619a252e600bed183a5b0f195eb728abaeb0b4f5
Muhammad Iqbal Rifai <iqbalrifai@waifu.club>
2018-09-28 03:28:45 +0700

    Update gen.php

--- gen.php
+++ gen.php
@@ -3,12 +3,11 @@
 	include "curl_gd.php";
 
 	if($_POST['submit'] != ""){
-		$url = $_POST['id'];
+		$url = $_POST['url'];
 		$gid = get_drive_id($url);
 		$iframeid = my_simple_crypt($gid);
 		$linkdown = Drive($url);
 		$file = '[{"type": "video/mp4", "label": "HD", "file": "'.$linkdown.'"}]';
-		$kontol = $_SERVER['SERVER_NAME'];
 	}
 ?>
 <!doctype html>
@@ -16,6 +15,14 @@
 <head>
   <meta charset="utf-8" />
 	<title>GDrive Gen</title>
+	 <link rel="stylesheet" href="https://bootswatch.com/4/litera/bootstrap.min.css">
+	<style>
+	.info-icon{margin-top:-6px}
+	.navbar-default {background: #fff !important;}
+	body { background: url('https://0wo.me/img/bg2.jpg') no-repeat center center fixed;-webkit-background-size:cover;-moz-background-size:cover;-o-background-size: cover;background-size: cover;}
+	.transp {background: rgba(255,255,255,0.5);}
+	.card{ background: rgba(255,255,255,0.9) !important; }
+	</style>
 </head>
 <body>
 
@@ -36,24 +43,28 @@
 		</form>
 		<div id="myElement">Paste the url and click the generate button.</div>
 
-
-<div class="col-md-12"><center>
+<div class="col-md-12">
 		<div class='card' style='margin:40px;'>
 		    <h5 class="card-header">Hasil Link Anda</h5>
 		    <div class="card-body">
 		    	<h6 class="text-muted">Silahkan Copy Link Anda.</h6>
 <textarea class="form-control" rows="6" readonly>
-<?php if($iframeid){echo 'http://'.$kontol.'?id='.$iframeid.'</textarea>';?></textarea><br/>
+<?php if($iframeid){echo '?id='.$iframeid.'</textarea>';?></textarea><br/>
+		<center>
 		<h2>Boleh di test link downloadnya dibawah</h2>
+		<a href="<?php echo $linkdown;?>"><button class="btn btn-default">Download</button></a></center>
 		<?php }?>
 </textarea>
 </div>
-	</div></center>
+	</div>
 
 
         <br><br>
   <section class="footme">
-      Google Drive Link Generator <a id="nochange" href="https://fb.me/iqbalrifaii/">iqbalrifai</a>
+      Google Drive Link Generator <a id="nochange" href="https://fb.me/iqbalrifaii">Iqbal Rifai</a>
   </section>
+
+<!--- 0wo text --->
+<script src="https://0wo.me/0wo.js"></script>
 </body>
 </html>
